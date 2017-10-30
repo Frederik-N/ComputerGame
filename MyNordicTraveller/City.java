dfa732058e19c936b36142b3eac7d4c36ba01d05
Dev.Nielsen <Frederik-N@hotmail.dk>
2017-10-30 13:23:07 +0100

    Add files via upload

---
+++ MyNordicTraveller/City.java
@@ -0,0 +1,33 @@
+
+/**
+ * Lav en beskrivelse af klassen City her.
+ * 
+ * @author (dit navn her)
+ * @version (versions nummer eller dato her)
+ */
+public class City
+{
+    // instansvariabler - erstat eksemplet herunder med dine egne variabler
+    private int x;
+
+    /**
+     * Konstruktør for objekter af klassen City
+     */
+    public City()
+    {
+        // initialiser instansvariable
+        x = 0;
+    }
+
+    /**
+     * Et eksempel på en metode - erstat denne kommentar med din egen
+     * 
+     * @param  y  eksempel på en parameter til en metode
+     * @return    summen af x og y 
+     */
+    public int sampleMethod(int y)
+    {
+        // indsæt din egen kode her
+        return x + y;
+    }
+}
---
+++ MyNordicTraveller/Road.java
@@ -0,0 +1,33 @@
+
+/**
+ * Lav en beskrivelse af klassen Road her.
+ * 
+ * @author (dit navn her)
+ * @version (versions nummer eller dato her)
+ */
+public class Road
+{
+    // instansvariabler - erstat eksemplet herunder med dine egne variabler
+    private int x;
+
+    /**
+     * Konstruktør for objekter af klassen Road
+     */
+    public Road()
+    {
+        // initialiser instansvariable
+        x = 0;
+    }
+
+    /**
+     * Et eksempel på en metode - erstat denne kommentar med din egen
+     * 
+     * @param  y  eksempel på en parameter til en metode
+     * @return    summen af x og y 
+     */
+    public int sampleMethod(int y)
+    {
+        // indsæt din egen kode her
+        return x + y;
+    }
+}
