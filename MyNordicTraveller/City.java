a9a31f31013990fd2604fcad2245c2f9a77db332
jona523c <jonas.madsen2@post.au.dk>
2017-11-09 11:34:49 +0100

    Comments for city

--- MyNordicTraveller/City.java
+++ MyNordicTraveller/City.java
@@ -12,22 +12,38 @@ public class City implements Comparable<City> {
         this.country = country;
     }
 
+    /**
+     * @return value for this city
+     */
     public int getValue() {
         return value;
     }
 
+    /**
+     * @return name for this city
+     */
     public String getName() {
         return name;
     }
 
+    /**
+     * @return country for this city
+     */
     public Country getCountry() {
         return country;
     }
 
+    /**
+     *  Adds a specific amount to value
+     * @param amount the amount to add to value
+     */
     public void changeValue(int amount) {
         value += amount;
     }
 
+    /**
+     * Sets value to its initial value
+     */
     public void reset() {
         value = initialValue;
     }
@@ -36,6 +52,10 @@ public class City implements Comparable<City> {
         return this.name.compareTo(c.name);
     }
 
+    /**
+     * Rolls a random number(bonus) and subtracts it from value.
+     * @return bonus that were substracted from value
+     */
     public int arrive() {
         int bonus = country.bonus(value);
         value -= bonus;
