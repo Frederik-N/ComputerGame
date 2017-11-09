e9fb5f3f7dee9c29db32d2f136f2716bee191c1c
jona523c <jonas.madsen2@post.au.dk>
2017-11-09 11:54:50 +0100

    Comments for position and road

--- MyNordicTraveller/Position.java
+++ MyNordicTraveller/Position.java
@@ -12,22 +12,38 @@ public class Position {
         this.total = distance;
     }
 
+    /**
+     * @return the city you move from
+     */
     public City getFrom() {
         return from;
     }
 
+    /**
+     * @return the city you're moving to
+     */
     public City getTo() {
         return to;
     }
 
+    /**
+     * @return the distance left to the "to" city
+     */
     public int getDistance() {
         return distance;
     }
 
+    /**
+     * @return the total distance between the cities
+     */
     public int getTotal() {
         return total;
     }
 
+    /**
+     * Checks if the player has arrived at the city.
+     * @return true if at the city, false if not
+     */
     public boolean hasArrived() {
         if(distance==0) {
             return true;
@@ -35,6 +51,10 @@ public class Position {
         return false;
     }
 
+    /**
+     * Moves 1 closer to the "to" city.
+     * @return true if distance above 0, false if not
+     */
     public boolean move() {
         if(distance>0) {
             distance--;
@@ -43,6 +63,9 @@ public class Position {
         return false;
     }
 
+    /**
+     * Turns the player around, switching the "to" city and the "from" city
+     */
     public void turnAround() {
         distance = Math.abs(distance-total);
         City nTo = to;
