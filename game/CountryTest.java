5bb22f16a8dff13018d137d84765c2267a2d0953
Frederik-N <Frederik-N@hotmail.dk>
2017-11-18 20:54:56 +0100

    constructortest

--- game/CountryTest.java
+++ game/CountryTest.java
@@ -6,7 +6,7 @@ import static org.junit.Assert.*;
 public class CountryTest {
     private Game game;
     private Country country1, country2;
-    private City cityA, cityB, cityC, cityE, cityF, cityG;
+    private City cityA, cityB, cityC, cityD, cityE, cityF, cityG;
     private Map<City, List<Road>> network1, network2;
 
     @Before
@@ -24,6 +24,7 @@ public class CountryTest {
         cityA = new City("City A", 80, country1);
         cityB = new City("City B", 60, country1);
         cityC = new City("City C", 70, country1);
+        cityD = new City("City D", 100, country1);
         cityE = new City("City E", 50, country2);
         cityF = new City("City F", 90, country2);
         cityG = new City("City G", 100, country2);
@@ -31,6 +32,7 @@ public class CountryTest {
         List<Road> roadsA = new ArrayList<>(),
                 roadsB = new ArrayList<>(),
                 roadsC = new ArrayList<>(),
+                roadsD = new ArrayList<>(),
                 roadsE = new ArrayList<>(),
                 roadsF = new ArrayList<>(),
                 roadsG = new ArrayList<>();
@@ -38,12 +40,23 @@ public class CountryTest {
         network1.put(cityA, roadsA);
         network1.put(cityB, roadsB);
         network1.put(cityC, roadsC);
+        network1.put(cityD, roadsD);
         network2.put(cityE, roadsE);
         network2.put(cityF, roadsF);
         network2.put(cityG, roadsG);
 
-
-        country2.addRoads(cityG, cityF, 4);
+        country1.addRoads(cityA, cityB, 4);
+        country1.addRoads(cityA, cityC, 3);
+        country1.addRoads(cityA, cityD, 5);
+        country1.addRoads(cityB, cityD, 2);
+        country1.addRoads(cityC, cityD, 2);
+        country1.addRoads(cityC, cityE, 4);
+        country1.addRoads(cityD, cityF, 3);
+        country2.addRoads(cityE, cityC, 4);
+        country2.addRoads(cityE, cityF, 2);
+        country2.addRoads(cityE, cityG, 5);
+        country2.addRoads(cityF, cityD, 3);
+        country2.addRoads(cityF, cityG, 6);
     }
 
     @Test
@@ -69,32 +82,33 @@ public class CountryTest {
         for(int seed = 0; seed<1000; seed++) {
             game.getRandom().setSeed(seed);
             int sum = 0;
-            int sum1 = 0;
-            int sum0 = 0;
             Set<Integer> values = new HashSet<>();
             Set<Integer> values1 = new HashSet<>();
-            Set<Integer> values0 = new HashSet<>();
             for(int i = 0; i<10000; i++) {
                 int bonus = country1.bonus(80);
                 int bonus1 = country1.bonus(1);
-                int bonus0 = country1.bonus(0);
+                boolean test = false;
+                if(bonus1==0 || bonus1==1) {
+                    test = true;
+                }
+                assertTrue(test);
+                assertEquals(country1.bonus(0), 0);
                 assertTrue(0<=bonus && bonus<=80);
-                assertTrue(0<=bonus1 && bonus1<=1);
-                assertTrue(bonus0==0);
                 sum += bonus;
-                sum1 += bonus1;
-                sum0 += bonus0;
                 values.add(bonus);
                 values1.add(bonus1);
-                values0.add(bonus0);
             }
             assertTrue(375000< sum && sum < 475000);
             assertEquals(values.size(),81);
-            assertTrue(4000< sum1 && sum1 < 6000);
             assertEquals(values1.size(), 2);
-            assertTrue(sum0==0);
-            assertEquals(values0.size(), 1);
         }
+
+
+        boolean test = false;
+        if(country1.bonus(1)==0 || country1.bonus(1)==1) {
+            test = true;
+        }
+        assertTrue(test);
     }
 
     @Test
@@ -115,7 +129,7 @@ public class CountryTest {
         assertEquals(country1.getRoads(cityA), networkA);
         assertEquals(country2.getRoads(cityE), networkE);
 
-        country1.addRoads(cityG, cityF, 6);
+        country1.addRoads(cityG, cityF, 4);
         assertEquals(country2.getRoads(cityF),networkF);
         assertEquals(country2.getRoads(cityG),networkG);
     }
@@ -139,7 +153,7 @@ public class CountryTest {
 
         assertEquals(country2.readyToTravel(cityG, cityF).getFrom(), cityG);
         assertEquals(country2.readyToTravel(cityG, cityF).getTo(), cityF);
-        assertEquals(country2.readyToTravel(cityG, cityF).getDistance(), 4);
+        assertEquals(country2.readyToTravel(cityG, cityF).getDistance(), 6);
 
         assertEquals(country1.readyToTravel(cityB, cityC).getFrom(), cityB);
         assertEquals(country1.readyToTravel(cityB, cityC).getTo(), cityB);
@@ -165,7 +179,7 @@ public class CountryTest {
     @Test
     public void getCities() throws Exception {
         List<City> cities = new ArrayList<>();
-        cities.add(cityA); cities.add(cityB); cities.add(cityC);
+        cities.add(cityA); cities.add(cityB); cities.add(cityC); cities.add(cityD);
         assertEquals(country1.getCities(), cities);
     }
 
